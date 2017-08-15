c6116247d615e108eeaf65cb7a5ec4085ed23082
Sauli Ketola <sauli.ketola@nitor.fi>
2017-08-15 15:51:44 +0300

    fb login + page access token creation in server

---
+++ index.js
@@ -0,0 +1,43 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var request = require('request')
+
+var app = express()
+
+app.use(express.static('static'))
+app.use(bodyParser.json()) // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })) // support encoded bodies
+
+var appSecret = 'APP-SECRET-HERE'
+
+// the web page will post here the user token / short lived token
+app.post('/access_token', function (req, res) {
+  console.log('Short lived token: ' + req.body.token)
+  console.log('User Id: ' + req.body.userId)
+
+  var token = req.body.token
+  var userId = req.body.userId
+  var url = 'https://graph.facebook.com/v2.10/oauth/access_token?grant_type=fb_exchange_token&client_id=349839645446838&client_secret='+appSecret+'&fb_exchange_token='+token
+
+  // request the long lived token
+  request.get(url,
+    function (error, response, body) {
+      var longLivedToken = JSON.parse(body).access_token
+      console.log('Long lived token: ' + longLivedToken)
+
+      var pageTokenUrl = 'https://graph.facebook.com/v2.10/'+userId+'/accounts?access_token=' + longLivedToken
+      // request the eternal page access token
+      request.get(pageTokenUrl,
+        function(error, response, body){
+          var eternalToken = JSON.parse(body).data[0].access_token
+          console.log('****************** Eternal token: **************')
+          console.log(eternalToken)
+          console.log('************************************************')
+          res.send('Great success')
+        }
+      )
+    }
+  )
+})
+
+app.listen(3000)
